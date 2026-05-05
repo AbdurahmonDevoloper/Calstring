81ab9a4c7f9254e108a2f27a13d0d10142d32b99
AbdurahmonDevoloper <abduvohidovabduraxmon86@gmail.com>
2026-05-05 06:45:35 +0500

    Loyiha  qo'shildi

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ifodani kiriting: ");
+        string input = Console.ReadLine();
+
+        int i = 0;
+        int result = 0;
+        int currentNumber = 0;
+        char operation = '+';
+
+        while (i < input.Length)
+        {
+            char c = input[i];
+
+            // 1. Raqam yig‘ish
+            if (c >= '0' && c <= '9')
+            {
+                currentNumber = currentNumber * 10 + (c - '0');
+            }
+
+            // 2. Operator yoki oxir bo‘lsa hisoblash
+            if (c == '+' || c == '-' || i == input.Length - 1)
+            {
+                if (operation == '+')
+                {
+                    result += currentNumber;
+                }
+                else if (operation == '-')
+                {
+                    result -= currentNumber;
+                }
+
+                operation = c;
+                currentNumber = 0;
+            }
+
+            i++;
+        }
+
+        Console.WriteLine("Natija: " + result);
+    }
+}
